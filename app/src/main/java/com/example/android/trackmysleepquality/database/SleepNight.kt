e592093e74df53e3bf5c8cda0c80eb2f1c8e7a99
Vlad Rynchenko <rynchenko.vlad1408@gmail.com>
2021-06-29 19:57:15 +0300

    Add Room

---
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
@@ -0,0 +1,17 @@
+package com.example.android.trackmysleepquality.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "daily_sleep_quality_table")
+data class SleepNight(
+    @PrimaryKey(autoGenerate = true)
+    var nightId: Long = 0L,
+    @ColumnInfo(name = "start_time_milli")
+    val startTimeInMillis: Long = System.currentTimeMillis(),
+    @ColumnInfo(name = "end_time_milli")
+    var endTimeMillis: Long = startTimeInMillis,
+    @ColumnInfo(name = "quality_rating")
+    var sleepQuality: Int = -1
+)
